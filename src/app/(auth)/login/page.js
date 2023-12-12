4f3b346784701969a4ac5a9b6dac503274645c6d
SamarthParmanand <parmanandsamarth@gmail.com>
2023-12-12 23:42:29 +0530

    add: login ui

--- src/app/(auth)/login/page.js
+++ src/app/(auth)/login/page.js
@@ -10,20 +10,34 @@ const page = () => {
   };
 
   return (
-    <div className="h-[95vh] bg-black/20 py-[12.5vh]">
-    <h1 className="text-center text-4xl my-3 font-bold">Welcome to ESG Edu!</h1>
-      <Card className=" flex flex-col justify-center items-center w-[40%] text-center mx-auto h-[40vh] p-4 border-2 border-black/25 bg-white/50 drop-shadow-2xl">
-        <CardTitle>Sign-In with Google</CardTitle>
-        <CardContent className="my-5 mx-3">
-          <button
-            className="flex justify-between border-2 mx-1 border-black/25 font-bold rounded-lg py-2 px-3 bg-violet-400 hover:bg-violet-500 transition-colors ease-linear"
-            onClick={handleClick}
-          >
-            <FaGoogle className="translate-y-1 mr-3" />
-            Sign-In
-          </button>
-        </CardContent>
-      </Card>
+    <div className="h-[95vh] bg-black/20 py-[12.5vh] flex">
+      <div className="w-[50%]">
+        <h1 className="text-center text-4xl my-10 font-bold">
+          Welcome to ESG Edu!
+        </h1>
+        <Card className="flex flex-col justify-center items-start w-[80%] text-center mx-auto h-[40vh] p-8 border-2 border-black/25 bg-white/50 drop-shadow-2xl">
+          <CardTitle>Login to ESG Edu</CardTitle>
+          <p className="text-gray-500 mt-3 text-left">
+            ESG Edu is a solution for Jharkhand Government.
+          </p>
+          <p className="text-gray-500 mb-3 text-left">
+            Login to get access to the management dashboard.
+          </p>
+          <CardContent className="my-5 px-0">
+            <button
+              className="flex justify-between w-full border-2 border-black/25 font-bold rounded-lg py-2 px-8 bg-violet-400 hover:bg-violet-500 transition-colors ease-linear"
+              onClick={handleClick}
+            >
+              <FaGoogle className="translate-y-1 mr-2" />
+              Sign-In
+            </button>
+          </CardContent>
+        </Card>
+      </div>
+      <div className="w-[50%] border-2 border-violet-500/50 mr-10">
+        {" "}
+        image here{" "}
+      </div>
     </div>
   );
 };
---
+++ src/app/dashboard/layout.js
@@ -0,0 +1,22 @@
+import { Inter } from "next/font/google";
+import "../global.css";
+import Sidebar from "@/components/Sidebar";
+import { Toaster } from "react-hot-toast";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "ESG-Edu",
+  description: "Smart India Hackathon 2023",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Sidebar>{children}</Sidebar>
+        <Toaster />
+      </body>
+    </html>
+  );
+}
