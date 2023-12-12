dac7b85c803a82a151c7cb43e9789f13ba7a057d
SamarthParmanand <parmanandsamarth@gmail.com>
2023-12-12 21:05:47 +0530

    feat: dashboard overview init

--- src/app/(auth)/login/page.js
+++ src/app/(auth)/login/page.js
@@ -1,9 +1,31 @@
-import React from 'react'
+"use client";
+
+import React from "react";
+import { Card, CardTitle, CardContent } from "@/components/ui/card";
+import { FaGoogle } from "react-icons/fa";
 
 const page = () => {
-  return (
-    <div>Login Page</div>
-  )
-}
+  const handleClick = () => {
+    console.log("handleClick");
+  };
 
-export default page
+  return (
+    <div className="h-[95vh] bg-black/20 py-[12.5vh]">
+    <h1 className="text-center text-4xl my-3 font-bold">Welcome to ESG Edu!</h1>
+      <Card className=" flex flex-col justify-center items-center w-[40%] text-center mx-auto h-[40vh] p-4 border-2 border-black/25 bg-white/50 drop-shadow-2xl">
+        <CardTitle>Sign-In with Google</CardTitle>
+        <CardContent className="my-5 mx-3">
+          <button
+            className="flex justify-between border-2 mx-1 border-black/25 font-bold rounded-lg py-2 px-3 bg-violet-400 hover:bg-violet-500 transition-colors ease-linear"
+            onClick={handleClick}
+          >
+            <FaGoogle className="translate-y-1 mr-3" />
+            Sign-In
+          </button>
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default page;
--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,9 +1,46 @@
-import React from 'react'
+"use client";
+import React, { useLayoutEffect } from "react";
+import { EsgSDK } from "esg-sdk";
+import { LineChart, DonutChart } from "@tremor/react";
+
+const ESG = EsgSDK.initialize();
+
+const DUMMY_VIEWS = [
+  { date: "Jan 01", views: 100 },
+  { date: "Feb 01", views: 200 },
+  { date: "Mar 01", views: 300 },
+  { date: "Apr 01", views: 250 },
+  { date: "May 01", views: 325 },
+];
 
 const page = () => {
+  // useLayoutEffect(() => {
+  //   const fetchData = async () => {
+  //     const data = await ESG.getViewsByDate();
+  //     console.log(data);
+  //   };
+  //   fetchData();
+  // }, [])
   return (
-    <div>Dashboard overview here @Hariharan</div>
-  )
-}
+    <div className="h-[95vh] bg-violet-100 flex flex-col items-center p-4">
+      <h1 className="text-3xl font-bold text-center">Dashboard Overview</h1>
+      <LineChart
+        data={DUMMY_VIEWS}
+        className="my-6"
+        index="date"
+        categories={["views"]}
+        color="blue"
+        yAxisWidth={30}
+      />
+      <DonutChart
+        data={DUMMY_VIEWS}
+        colors={["blue", "green", "red", "yellow", "amber"]}
+        index="date"
+        category="views"
+        className="my-10"
+      />
+    </div>
+  );
+};
 
-export default page
+export default page;
